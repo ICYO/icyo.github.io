5807899691bee0ef02a78cd0b1324bfd768442c6
blldxt <blldxt@live.com>
2017-11-18 23:59:01 +0000

    icyo

---
+++ resume/script/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    function dataURLtoBlob(dataurl) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {type:mime});
+    }
+
+    var btn = document.getElementById("download-image");
+    var dom = document.getElementById("main");
+    btn.addEventListener("click", function(event) {
+        html2canvas(dom).then(function(canvas) {
+            var blob = dataURLtoBlob(canvas.toDataURL('image/png', 1))
+            var aLink = document.createElement('a');
+            aLink.download = "resume.png";
+            aLink.href = URL.createObjectURL(blob);
+            console.log(aLink.href);
+            aLink.click();
+        });
+    });
+});
