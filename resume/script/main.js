9f6db25512ea064923b3794a802cf3f966f5ad59
blldxt <blldxt@live.com>
2017-11-19 11:43:07 +0000

    icyo

--- resume/script/main.js
+++ resume/script/main.js
@@ -1,20 +1,6 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    if (document.body.clientWidth < 600) {
-        var link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.type = "text/css";
-        link.href="/resume/style/mobile.css";
-        document.head.appendChild(link);
-    } else {
-        var link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.type = "text/css";
-        link.href="/resume/style/main.css";
-        document.head.appendChild(link);
-    }
-
     function dataURLtoBlob(dataurl) {
         var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
             bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
@@ -28,11 +14,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var dom = document.getElementById("main");
     btn.addEventListener("click", function(event) {
         html2canvas(dom).then(function(canvas) {
-            var blob = dataURLtoBlob(canvas.toDataURL('image/png', 1))
+            document.body.appendChild(canvas);
+            try {
+                var blob = dataURLtoBlob(canvas.toDataURL('image/png', 1))
+            } catch(e) {
+                alert(e);
+            }
             var aLink = document.createElement('a');
             aLink.download = "resume.png";
             aLink.href = URL.createObjectURL(blob);
-            console.log(aLink.href);
+            alert(aLink.href);
             aLink.click();
         });
     });
